9c94b4139210e8dcebd29eab7ea6bf34f2949249
Michael <michael.stillwell@hotmail.com>
2018-05-22 16:35:08 -0500

    Initialized ideas

---
+++ client/src/components/Enemy/Enemy.js
@@ -0,0 +1,10 @@
+// Import React
+import React, { Component } from 'react';
+
+export default class Enemy {
+    constructor(hp, color, speed) {
+        this.hp = hp;
+        this.color = color;
+        this.speed = speed;
+    }
+}
---
+++ client/src/components/Track/Track.js
@@ -0,0 +1,9 @@
+'use strict';
+export default class Track {
+    constructor(type, direction) {
+        this.type = type;
+        this.direction = direction;
+    }
+
+
+};
